fbd550e9433cfda7885a50626c1dc48047b61056
Euliya <zhuyihongmaggie@gmail.com>
2024-02-01 14:25:05 -0500

    Moving plane and update path.

--- Assets/Week 4/Scripts/Plane.cs
+++ Assets/Week 4/Scripts/Plane.cs
@@ -6,20 +6,51 @@ using UnityEngine.UIElements;
 
 public class Plane : MonoBehaviour
 {
-
-
     public List<Vector2> points;
     public float newPointThreshold = 0.2f;
     Vector2 lastPosition;
     LineRenderer lineRenderer;
+    Rigidbody2D rigidbody;
+    Vector2 currentPosition;
+    public float speed = 1;
 
     void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
         lineRenderer.positionCount = 1;
         lineRenderer.SetPosition(0,transform.position);
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
+    private void FixedUpdate()
+    {
+        currentPosition = transform.position;
+        if(points.Count > 0 )
+        {
+            Vector2 direction = points[0] - currentPosition;
+            float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+            rigidbody.rotation = -angle;
+        }
+        rigidbody.MovePosition(rigidbody.position + (Vector2)transform.up * speed * Time.deltaTime);
+    }
+    void Update()
+    {
+        lineRenderer.SetPosition(0, transform.position);
+        if (points.Count > 0 )
+        {
+            if (Vector2.Distance(currentPosition, points[0]) < newPointThreshold)
+            {
+                points.RemoveAt(0);
+
+                for (int i = 0; i < lineRenderer.positionCount - 2; i++)
+                {
+                    lineRenderer.SetPosition(i, lineRenderer.GetPosition(i + 1));
+                }
+                lineRenderer.positionCount--;
+            } 
+        }
+
+    }
     private void OnMouseDown()
     {
         points= new List<Vector2>();
@@ -37,11 +68,5 @@ public class Plane : MonoBehaviour
             lineRenderer.SetPosition(lineRenderer.positionCount - 1, newPosition);
             lastPosition = newPosition;
         }
-    }
-    
-    void Update()
-    {
-        
-
-    }
+    }  
 }
