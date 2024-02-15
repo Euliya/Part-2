390437c49f8fc775dd0f7aa0c4fa0ba26f2194d5
Euliya <zhuyihongmaggie@gmail.com>
2024-02-15 13:43:11 -0500

    Fixed death movement.

--- Assets/Week 5/Scripts/Knight.cs
+++ Assets/Week 5/Scripts/Knight.cs
@@ -12,8 +12,8 @@ public class Knight : MonoBehaviour
     bool clickingOnSelf = false;
     public float health;
     public float maxHealth = 5;
-    //public HealthBar healthBar;
-    
+    bool isDead = false;
+
 
     // Start is called before the first frame update   
     void Start()
@@ -25,6 +25,8 @@ public class Knight : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (isDead) return; 
+        
         movement = destination - (Vector2)transform.position;
 
         if (movement.magnitude < 0.1)
@@ -37,6 +39,8 @@ public class Knight : MonoBehaviour
 
     void Update()
     {
+        if (isDead) return;
+
         if (Input.GetMouseButtonDown(0) && !clickingOnSelf)
         {
             destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -51,6 +55,7 @@ public class Knight : MonoBehaviour
 
     private void OnMouseDown()
     {
+        if (isDead) return;
         clickingOnSelf = true;
         SendMessage("TakeDamage", 1);
     }
@@ -65,11 +70,12 @@ public class Knight : MonoBehaviour
         health = Mathf.Clamp(health, 0, maxHealth);
         if (health == 0)
         {
-            
+            isDead = true;
             animator.SetTrigger("Death");
         }
         else
         {
+            isDead = false;
             animator.SetTrigger("TakeDamage");
         }
         
